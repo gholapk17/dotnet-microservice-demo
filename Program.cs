6f458a5514974a67f83ade6e33007809ba70a75b
gholapk17 <root@ip-172-31-26-46.ap-south-1.compute.internal>
2025-09-27 18:16:07 +0000

    Add /hello endpoint to show custom message

--- Program.cs
+++ Program.cs
@@ -40,6 +40,8 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.MapGet("/hello", () => "Hello from Jenkins Build!");
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
